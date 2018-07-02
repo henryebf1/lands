e1f9904072c9e22e362e65ab49937b5a7032bd17
ARTEAGA PEREZ ENRIQUE <enrique.arteagap@my.uvm.edu.mx>
2018-07-01 21:04:28 -0500

    primera parte del menu

---
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,107 @@
+﻿namespace Lands.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel : BaseViewModels
+    {
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Propierties
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get { return password; }
+            set { SetValue(ref password, value); }
+        }
+        public bool IsRunning
+        {
+
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.IsEnabled = true;
+        }
+
+        #endregion
+
+        #region Command
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email!",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password!",
+                    "Accept");
+                return;
+            }
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jzuluaga55@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password incorrect!",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                    "ok",
+                    "fuck yeah!",
+                    "Accept");
+            return;
+        }
+
+        #endregion
+    }
+}
